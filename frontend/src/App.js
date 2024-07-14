9190d21af393f6ebb3226b1b4d0e3b8f0d73b4a6
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-07-13 22:49:14 -0400

    router links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import  Header  from "./components/Header";
 import  Footer  from "./components/Footer";
-import { Outlet } from "react-router-dom";
+
 
 
 
@@ -14,7 +15,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           {/* <h1>Spinmasters welcomes you</h1> */}
-
+            <Outlet />
         </Container>
         
       </main>
