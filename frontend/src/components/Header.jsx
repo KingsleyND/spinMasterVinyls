e0555eb869d0da3a7e22337d02be3945effbe0d5
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-07-14 23:47:32 -0400

    cart and product routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar,Nav, Container} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
 import smLogo from '../assets/smLogo.png';
+import {LinkContainer} from 'react-router-bootstrap';
 
 
 export const Header = () => {
@@ -9,14 +10,21 @@ export const Header = () => {
     <header>
         <Navbar bg='dark' variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>
+                <LinkContainer to='/'>
+                <Navbar.Brand>
                     <img src={smLogo} alt="spinmastersLogo" width="50px" height="50px"/>
-                    SpinMasters</Navbar.Brand>
+                    SpinMasters
+                </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
-                        <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
+                        <LinkContainer to='/cart'>
+                        <Nav.Link><FaShoppingCart/>Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
                         <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
