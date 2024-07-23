5131bcddfd8fdfea5696d858b4b0b2706a9c1606
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-07-22 23:06:43 -0400

    product details initialization

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import products from '../products'
+
+const ProductScreen = () => {
+    const { id } = useParams()
+  return (
+    <div>ProductScreen</div>
+  )
+}
+
+export default ProductScreen
