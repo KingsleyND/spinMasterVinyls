c1c7708f5aa28ed6b045f4ea0056030949e3dede
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-07-24 21:51:10 -0400

    dynamic route for product details initialization

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import products from '../products'
 
 const ProductScreen = () => {
-    const { id } = useParams()
+    const { id: productId } = useParams() //returns an object with key id (id is renamed to productId)
   return (
     <div>sProductScreen</div>
   )
