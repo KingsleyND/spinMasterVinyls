77c563c16f4c8b089b87f503da46cf0938e3b2f6
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-07-27 05:52:07 -0400

    product detail routing

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -4,6 +4,8 @@ import products from '../products'
 
 const ProductScreen = () => {
     const { id: productId } = useParams() //returns an object with key id (id is renamed to productId)
+    const product = products.find(product => product._id === productId) //find the product with the same id as the productId
+    console.log(product)
   return (
     <div>sProductScreen</div>
   )
