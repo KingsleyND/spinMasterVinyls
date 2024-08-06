789f7d27058c4437aee3cae607cef398a7c95723
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2024-08-05 23:51:16 -0400

    local data express retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';  // Import express using ES6 syntax
-
+import products from './data/products.js';  // Import the products data
 const port = 5000;  // Define the port number (frontend uses 3000)
 
 const app = express();  
 
 app.get('/', (req, res) => {  // Define a route for the root URL
     res.send('Server is running..');  // Send a response to the client
+});
+
+app.get('/api/products', (req, res) => {  // Define a route for the products data
+    res.json(products);  // Send the products data to the client
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));  
