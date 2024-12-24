58f6997c9646c0009eb7d426756f1b30d2ef5836
KingsleyND <72080248+KingsleyND@users.noreply.github.com>
2024-12-24 14:35:32 -0500

    refr(express)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          {/* <h1>Spinmasters welcomes you</h1> */}
+          {/* <h1>Spinmasters welcomes you.</h1> */}
             <Outlet />
         </Container>
         
